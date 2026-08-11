f3fdb0d95505e241e87bfc5af81a497d45183526
Bowen Weeks <weeksbo@oregonstate.edu>
2026-08-11 16:45:12 -0700

    Laptop crashed save

--- Scripts/Data_Script.R
+++ Scripts/Data_Script.R
@@ -2,9 +2,6 @@
 library(sf)
 library(dplyr)
 library(ggplot2)
-# Load cleaned data
-slido_time <- readRDS("C:/OSU/CS_458/Data/Clean_Data/slido_time.rds")
-slido_map <- readRDS("C:/OSU/CS_458/Data/Clean_Data/slido_map.rds")
 
 yearly_counts <- slido_time %>%
   st_drop_geometry() %>%
